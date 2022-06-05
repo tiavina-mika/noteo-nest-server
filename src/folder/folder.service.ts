fab0fe323db70ff3bbba66f80c762e9db732cd92
tiavina-mika <tiavinamika@gmail.com>
2022-06-05 18:21:10 +0200

    add folder module

---
+++ src/folder/folder.service.ts
@@ -0,0 +1,39 @@
+import { Model } from 'mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateFolderInput, UpdateFolderInput } from './folder.input';
+import { Folder, FolderDocument } from './folder.schema';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class FolderService {
+  constructor(
+    @InjectModel(Folder.name) private folderModel: Model<FolderDocument>
+  ) {}
+
+  async create(values: CreateFolderInput): Promise<Folder> {
+    const createdFolder = new this.folderModel(values);
+    return createdFolder.save();
+  }
+
+  async findAll(): Promise<Folder[]> {
+    return await this.folderModel.find().exec();
+  }
+
+  async getById(id: string): Promise<Folder> {
+    const folder = await this.folderModel.findById(id).exec();
+    if (!folder) {
+      throw new NotFoundException(id);
+    }
+    return folder;
+  }
+
+  async update(values: UpdateFolderInput) {
+    return await this.folderModel
+      .findByIdAndUpdate(values.id, values, { new: true })
+      .exec();
+  }
+
+  async delete(id: string) {
+    return await this.folderModel.findByIdAndDelete(id).exec();
+  }
+}
