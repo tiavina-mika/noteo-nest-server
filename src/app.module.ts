cce8d1cd697fa2e6477d27a8274046ece5cee994
tiavina-mika <tiavinamika@gmail.com>
2022-06-12 12:39:07 +0200

    hash password

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteModule } from './note/note.module';
 import configuration from './env';
-import { DirectiveLocation, GraphQLDirective } from 'graphql';
+import {
+  DirectiveLocation,
+  GraphQLDirective,
+  GraphQLError,
+  GraphQLFormattedError,
+} from 'graphql';
 import { upperDirectiveTransformer } from './common/directive/upper-case.directive';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FolderModule } from './folder/folder.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { formatGraphQLErrorMessage } from './utils/errors';
 
 @Module({
   imports: [
@@ -30,6 +36,42 @@ import { UsersModule } from './users/users.module';
             locations: [DirectiveLocation.FIELD_DEFINITION],
           }),
         ],
+      },
+      formatError: (error: GraphQLError) => {
+        const customError = formatGraphQLErrorMessage(error);
+        if (customError) {
+          new Error(customError);
+        } else if (error.message === 'VALIDATION_ERROR') {
+          const extensions = {
+            code: 'VALIDATION_ERROR',
+            errors: [],
+          };
+
+          Object.keys(error.extensions.invalidArgs).forEach((key) => {
+            const constraints = [];
+            Object.keys(error.extensions.invalidArgs[key].constraints).forEach(
+              (_key) => {
+                constraints.push(
+                  error.extensions.invalidArgs[key].constraints[_key]
+                );
+              }
+            );
+
+            extensions.errors.push({
+              field: error.extensions.invalidArgs[key].property,
+              errors: constraints,
+            });
+          });
+
+          const graphQLFormattedError: GraphQLFormattedError = {
+            message: 'VALIDATION_ERROR',
+            extensions: extensions,
+          };
+
+          return graphQLFormattedError;
+        } else {
+          return error;
+        }
       },
     }),
     ConfigModule.forRoot({
