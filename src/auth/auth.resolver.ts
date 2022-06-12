5caa93e8a8813380d0101a6884dfd2501104c9ad
tiavina-mika <tiavinamika@gmail.com>
2022-06-12 12:39:07 +0200

    hash password

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
+import { BadRequestException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import * as bcrypt from 'bcryptjs';
 
 import { AuthService } from '../auth/auth.service';
 import { LoginInput } from './auth.input';
@@ -15,14 +16,28 @@ export class AuthResolver {
     private usersService: UsersService
   ) {}
 
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
   @Mutation((returns) => User)
   async login(@Args('values') values: LoginInput) {
-    return this.authService.validateUser(values);
+    const user = await this.usersService.findOneByEmail(
+      values.email.toLowerCase()
+    );
+
+    const isValidPassword = bcrypt.compareSync(values.password, user.password);
+
+    if (!isValidPassword) {
+      throw new BadRequestException('Password does not match');
+    }
+
+    return user;
   }
 
   @Mutation((returns) => User)
   async signup(@Args('values') values: CreateUserInput) {
-    return this.usersService.create(values);
+    const newValues = {
+      ...values,
+      password: bcrypt.hashSync(values.password, 10),
+    };
+    return this.usersService.create(newValues);
   }
 }
