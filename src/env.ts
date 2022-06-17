f2162a197939d805542734745d85274ae91979b6
tiavina-mika <tiavinamika@gmail.com>
2022-06-17 21:31:09 +0200

    remove unused files

--- src/env.ts
+++
@@ -1,8 +0,0 @@
-// env.ts
-export default () => ({
-  // Add your own properties here however you'd like
-  // port: parseInt(process.env.PORT, 10) || 3000,
-  database: {
-    host: process.env.DB_URL,
-  },
-});
